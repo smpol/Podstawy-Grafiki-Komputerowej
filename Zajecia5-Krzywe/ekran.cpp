c9c48639fb7bffe8d03faf5446b90dfeb3d6c23e
smpol <5789000+smpol@users.noreply.github.com>
2023-04-01 19:07:57 +0200

    Zrobiono Beziur

--- Zajecia5-Krzywe/ekran.cpp
+++ Zajecia5-Krzywe/ekran.cpp
@@ -8,45 +8,43 @@
 
 #define wysokosc 1000
 #define szerokosc 1000
-int temp=100;
-bool elipsa=false;
+int temp = 100;
+bool elipsa = false;
 
 QVector<QPointF> points;
-//std::vector<vector<int>> points_2d;
-//QVector<QVector<int>> points_2d;
+// std::vector<vector<int>> points_2d;
+// QVector<QVector<int>> points_2d;
 
-
-Ekran::Ekran(QWidget *parent)    : QWidget{parent}
+Ekran::Ekran(QWidget *parent) : QWidget{parent}
 {
-    im = QImage (szerokosc, wysokosc, QImage::Format_RGB32);
+    im = QImage(szerokosc, wysokosc, QImage::Format_RGB32);
     im.fill(0);
 
-    //im_copy=QImage (szerokosc, wysokosc, QImage::Format_RGB32);
-    //im_copy.fill(0);
-
+    //    im_copy = QImage(szerokosc, wysokosc, QImage::Format_RGB32);
+    //    im_copy.fill(0);
 }
 
 void Ekran::wartosc(int n)
 {
-    temp=n;
+    temp = n;
 }
 
 void Ekran::czysc()
 {
     im.fill(0);
+    //    im_copy.fill(0);
     points.clear();
     update();
 }
 
 void Ekran::tryb_elipsa()
 {
-    elipsa=true;
+    elipsa = true;
 }
 void Ekran::tryb_kolo()
 {
-    elipsa=false;
+    elipsa = false;
 }
-
 
 void Ekran::paintEvent(QPaintEvent *)
 {
@@ -55,32 +53,72 @@ void Ekran::paintEvent(QPaintEvent *)
 }
 
 // zmienne przechowujace koordynaty 1 punktu
-int startX=0;
-int startY=0;
-//bool elipsa=false;
+int startX = 0;
+int startY = 0;
+// bool elipsa=false;
 
 void Ekran::mousePressEvent(QMouseEvent *e)
 {
-    int startX=e->position().x();
-    int startY=e->position().y();
-    points<<QPointF(startX, startY);
-    if (points.size() >=4 && points.size()%3==1)
+    int startX = e->position().x();
+    int startY = e->position().y();
+    if (e->button() == Qt::LeftButton)
     {
-        im.fill(0);
-        qDebug("Wykonano, ilosc %d", (int)points.size());
-        drawBezier(points, 1000);
+        drawPixel(startX, startY);
+        points << QPointF(startX, startY);
+    }
+    else if (e->button() == Qt::RightButton)
+    {
+        int index = findNearestPoint(points, startX, startY);
+        if (index != -1)
+        {
+            points.remove(index);
+            im.fill(0);
+            for (int i = 0; i < points.size(); i++)
+            {
+                drawPixel(points[i].x(), points[i].y());
+            }
+        }
+    }
+    if (points.size() >= 4)
+    {
+        drawBezier(points, 5000);
     }
 
     update(); // odświeżanie okna
 }
 
-void Ekran::mouseMoveEvent(QMouseEvent *)
+int temp_index = -1;
+
+void Ekran::mouseMoveEvent(QMouseEvent *e)
 {
-
-    //im=im_copy;
-
+    if (e->buttons() == Qt::MiddleButton)
+    {
+        int tempX = e->position().x();
+        int tempY = e->position().y();
+        if (temp_index == -1)
+        {
+            temp_index = findNearestPoint(points, tempX, tempY);
+        }
+        if (temp_index != -1)
+        {
+            im.fill(0);
+            points[temp_index].setX(tempX);
+            points[temp_index].setY(tempY);
+            for (int i = 0; i < points.size(); i++)
+            {
+                drawPixel(points[i].x(), points[i].y());
+            }
+            if (points.size() >= 4)
+            {
+                drawBezier(points, 5000);
+            }
+        }
+    }
 
     update();
-
 }
 
+void Ekran::mouseReleaseEvent(QMouseEvent *)
+{
+    temp_index = -1;
+}
--- Zajecia5-Krzywe/ekran.h
+++ Zajecia5-Krzywe/ekran.h
@@ -24,6 +24,19 @@ public:
             pix[4 * x + 2] = 255;
         }
     }
+    void clearPixel(int x, int y)
+    {
+        int width = im.width();
+        int height = im.height();
+
+        if ((x >= 0) && (x <= width) && (y >= 0) && (y <= height))
+        {
+            uchar *pix = im.scanLine(y); // dostęp do piksela
+            pix[4 * x] = 0;            // blue
+            pix[4 * x + 1] = 0;        // green
+            pix[4 * x + 2] = 0;
+        }
+    }
     void drawLine(int x0, int y0, int x1, int y1)
     {
         double m = 0;
@@ -103,26 +116,55 @@ public:
         }
     }
 
-    void drawBezier(QVector<QPointF> &points, int k)
+    int findNearestPoint(QVector<QPointF> &points, int x, int y)
     {
-        //rysowanie krzywej beziera dla n punktów
-
-        QPointF temp1, temp2;
-        int z=0;
-        for (int i=0; i<k; i++)
+        int odlegosc = 20;//index of nearest point
+        int index=-1;
+        for (int i=0; i<points.size(); i++)
         {
-            float t = (float)i/(float)k;
-            temp1 = (1-t)*(1-t)*(1-t)*points[0+z] + 3*(1-t)*(1-t)*t*points[1+z] + 3*(1-t)*t*t*points[2+z] + t*t*t*points[3+z];
-            if (i==0)
+            if (sqrt((points[i].x()-x)*(points[i].x()-x) + (points[i].y()-y)*(points[i].y()-y)) < odlegosc)
             {
-                temp2 = temp1;
-            }
-            else
-            {
-                drawLine(temp1.x(), temp1.y(), temp2.x(), temp2.y());
-                temp2 = temp1;
+                odlegosc = sqrt((points[i].x()-x)*(points[i].x()-x) + (points[i].y()-y)*(points[i].y()-y));
+                index = i;
             }
         }
+        return index;
+    }
+
+    void drawBezier(QVector<QPointF> &points, int k)
+    {
+        //uzywajac drawPixel
+//        for (int i=0; i<points.size()-3; i+=3)
+//        {
+//            for (int j=0; j<k; j++)
+//            {
+//                float t = (float)j/(float)k;
+//                double x = (1-t)*(1-t)*(1-t)*points[0+i].x() + 3*(1-t)*(1-t)*t*points[1+i].x() + 3*(1-t)*t*t*points[2+i].x() + t*t*t*points[3+i].x();
+//                double y = (1-t)*(1-t)*(1-t)*points[0+i].y() + 3*(1-t)*(1-t)*t*points[1+i].y() + 3*(1-t)*t*t*points[2+i].y() + t*t*t*points[3+i].y();
+//                drawPixel(x, y);
+//            }
+//        }
+
+
+    //     uzywajac drawLine
+       QPointF temp1, temp2;
+       for (int i=0; i<points.size()-3; i+=3)
+       {
+           for (int j=0; j<k; j++)
+           {
+               float t = (float)j/(float)k;
+               temp1 = (1-t)*(1-t)*(1-t)*points[0+i] + 3*(1-t)*(1-t)*t*points[1+i] + 3*(1-t)*t*t*points[2+i] + t*t*t*points[3+i];
+               if (j==0)
+               {
+                   temp2 = temp1;
+               }
+               else
+               {
+                   drawLine(temp1.x(), temp1.y(), temp2.x(), temp2.y());
+                   temp2 = temp1;
+               }
+           }
+       }
     }
 
 public slots:
@@ -135,6 +177,7 @@ protected:
     void paintEvent(QPaintEvent *event);
     void mousePressEvent(QMouseEvent *);
     void mouseMoveEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *e);
 signals:
 private:
     QImage im;
